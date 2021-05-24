708df8aa628502d085f726ea4354024890198f45
Graham French <Graham.French@jncc.gov.uk>
2021-05-24 09:20:04 +0100

    added country_codes html table example

--- data-raw/lookup_raw.R
+++ data-raw/lookup_raw.R
@@ -18,6 +18,7 @@ country_codes <- country_codes %>%
     alpha_3 == "HKG" ~ "Asia",
     alpha_3 == "MAC" ~ "Asia",
     alpha_3 == "TWN" ~ "Asia",
+    alpha_3 == "ATA" ~ "Antarctica",
     TRUE ~ un_region_name)) %>%
   dplyr::bind_rows(c(alpha_3 = "YUG",
                      country_name = "Yugoslavia",
