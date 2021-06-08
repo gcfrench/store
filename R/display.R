864479c392597cc2841479e24c0d90b2c2eb53ab
Graham French <Graham.French@jncc.gov.uk>
2021-06-08 09:35:25 +0100

    updated reduce_image_size function documentation

--- R/display.R
+++ R/display.R
@@ -44,11 +44,14 @@ display_table <- function(data, rows = nrow(data)) {
 #' This function adds a border shadow to the image, archiving the original image
 #' in a separate archive sub-directory
 #'
-#' @seealso
+#' @details
 #' The example batch runs the function on a graph from the
 #' [palmerpenguins package](https://allisonhorst.github.io/palmerpenguins/articles/examples.html)
 #' and is an example of parallelization using the [future](https://github.com/HenrikBengtsson/future)
 #' and [furrr](https://davisvaughan.github.io/furrr/) packages
+#'
+#' @section Figures:
+#' ![Penguins mass v flipper shadow plot](penguins_mass_flipper_plot_shadow.png)
 #'
 #' @family image manipulation functions
 #'
@@ -87,11 +90,8 @@ display_table <- function(data, rows = nrow(data)) {
 #' })
 #'
 #' file_copy(path(tempdir(), "figures", "penguins_mass_flipper_plot.png"),
-#'           here("man", "figures", "penguins_mass_flipper_plot.png"),
+#'           here("man", "figures", "penguins_mass_flipper_plot_shadow.png"),
 #'           overwrite = TRUE)
-#'
-#' @section Figures:
-#' ![Penguins mass v flipper plot](penguins_mass_flipper_plot.png)
 add_image_shadow <- function(path_image) {
 
    # create archive directory if does not exist
@@ -118,16 +118,13 @@ add_image_shadow <- function(path_image) {
 #' Reduce the size of an image
 #'
 #' @description
-#' This function reduces the size of an image, archiving the
-#' original image in a separate archive sub-directory
+#' This function reduces the size of an image, archiving the original image in a
+#' separate archive sub-directory
 #'
-#' @seealso
-#' The example batch runs the function on a graph from the
-#' [palmerpenguins package](https://allisonhorst.github.io/palmerpenguins/articles/examples.html)
-#' and [palmer penguins image](https://allisonhorst.github.io/palmerpenguins/articles/art.html)
-#' from Allison Horst's Art work. Artwork by @allison_horst and is an example of parallelization using
-#' the [future](https://github.com/HenrikBengtsson/future) and [furrr](https://davisvaughan.github.io/furrr/)
-#' packages
+#' @inherit add_image_shadow return details
+#'
+#' @section Figures:
+#' ![Penguins mass v flipper reduced plot](penguins_mass_flipper_plot_reduced.png)
 #'
 #' @seealso [Jumping rivers](https://www.jumpingrivers.com/) has written a series of
 #' four blogs for [displaying images on web pages](https://www.jumpingrivers.com/blog/knitr-include-graphics-external/).
@@ -137,111 +134,41 @@ add_image_shadow <- function(path_image) {
 #' @inheritParams add_image_shadow
 #' @param image_size Percentage size reduction with 50% as the default
 #'
-#' @return The path name of image returned invisibly so that the function can be
-#' used in a piped workflow
+#' @inherit add_image_shadow return return
+#'
 #' @export
 #'
+#' @example man/examples/penguins_mass_flipper_plot.R
+#'
 #' @examples
-#' \dontrun{
+#' # batch reduce images
 #' suppressPackageStartupMessages({
-#'   library(store)
 #'   suppressWarnings({
-#'     library(palmerpenguins)
 #'     library(fs)
-#'     library(dplyr)
-#'     library(ggplot2)
-#'     library(here)
-#'     library(magick)
 #'     library(future)
 #'     library(furrr)
 #'   })
 #' })
-#' # create output directory
-#' i_am("example.Rmd")
-#' if (!dir_exists("output")) {dir_create("output")}
-#' if (!dir_exists("output/images")) {dir_create("output/images")}
 #'
-#' # example taken from palmerpenguins example analysis of mass vs. flipper length
-#' # https://allisonhorst.github.io/palmerpenguins/articles/examples.html
-#'
-#' # data
-#' mass_flipper_data <- penguins %>%
-#'   select(species, flipper_length_mm, body_mass_g, )
-#' mass_flipper_data
-#'
-#' # graph
-#' mass_flipper_graph <- ggplot(data = mass_flipper_data,
-#'                              aes(x = flipper_length_mm,
-#'                                  y = body_mass_g)) +
-#'   geom_point(aes(color = species,
-#'                  shape = species),
-#'              size = 3,
-#'              alpha = 0.8) +
-#'   theme_minimal() +
-#'   scale_color_manual(values = c("darkorange","purple","cyan4")) +
-#'   labs(title = "Penguin size, Palmer Station LTER",
-#'        subtitle = "Flipper length and body mass for Adelie, Chinstrap and Gentoo Penguins",
-#'        x = "Flipper length (mm)",
-#'        y = "Body mass (g)",
-#'        color = "Penguin species",
-#'        shape = "Penguin species") +
-#'   theme(legend.position = c(0.2, 0.7),
-#'         legend.background = element_rect(fill = "white", color = NA),
-#'         plot.title.position = "plot",
-#'         plot.caption = element_text(hjust = 0, face= "italic"),
-#'         plot.caption.position = "plot")
-#'
-#' # save graph
-#' ggsave(filename = here("output", "images", "palmerpenguins_graph.png"),
-#'        plot = mass_flipper_graph,
-#'        type = "cairo-png",
-#'        width = 8,
-#'        height = 8,
-#'        units = "in",
-#'        dpi = 72)
-#'
-#' # graph image
-#' mass_flipper_graph <- image_read(here("output", "images", "palmerpenguins_graph.png"))
-#' mass_flipper_graph
-#'
-#' # graph information
-#' image_info(mass_flipper_graph)
-#'
-#' # image taken from Allison Horst's Art for teaching with palmerpengiuns
-#' # Citation: Artwork by @allison_horst
-#' lter_penguins <- image_read("https://allisonhorst.github.io/palmerpenguins/reference/figures/lter_penguins.png")
-#' lter_penguins
-#'
-#' # image information
-#' image_info(lter_penguins)
-#'
-#' # save image
-#' image_write(image = lter_penguins,
-#'             path = here("output", "images", "palmerpenguins_image.png"))
-#'
-#' # batch reduce images
 #' plan(multisession)
-#' here("output/images") %>%
-#'   dir_ls(., glob = "*.png") %>%
-#'   future_map(reduce_image_size,
-#'              image_size = "50%",
-#'              .options = furrr_options(seed = TRUE),
-#'              .progress = TRUE)
+#' path(tempdir(), "figures") %>% {
+#'   suppressMessages({dir_ls(., glob = "*.png")})} %>%
+#'   future_walk(reduce_image_size,
+#'               image_size = "50%",
+#'               .options = furrr_options(seed = TRUE),
+#'               .progress = TRUE)
 #'
-#' # reduced graph
-#' mass_flipper_graph_reduce <- image_read(here("output", "images", "palmerpenguins_graph.png"))
-#' mass_flipper_graph_reduce
+#' # copy reduced graph image to man directory
+#' suppressPackageStartupMessages({
+#'    suppressWarnings({
+#'       library(fs)
+#'       library(here)
+#'    })
+#' })
 #'
-#' # reduced graph information
-#' image_info(mass_flipper_graph_reduce)
-#'
-#' # reduced image
-#' lter_penguins_reduce <- image_read(here("output", "images", "palmerpenguins_image.png"))
-#' lter_penguins_reduce
-#'
-#' # reduced image information
-#' image_info(lter_penguins_reduce)
-#' }
+#' file_copy(path(tempdir(), "figures", "penguins_mass_flipper_plot.png"),
+#'           here("man", "figures", "penguins_mass_flipper_plot_reduced.png"),
+#'           overwrite = TRUE)
  reduce_image_size <- function(path_image, image_size = "50%") {
 
    # create archive directory if does not exist
